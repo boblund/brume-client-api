16974d38fbdf029fda37fdc709dd996ef7eec282
Bob <bob.lund@gmail.com>
2025-07-25 06:21:34 -0600

    webpack fix, INFO default

--- logger.mjs
+++ logger.mjs
@@ -4,15 +4,15 @@ function basename( path ){ return path.split('/').reverse()[0]; };
 
 const levels = [ 'DEBUG', 'INFO', 'WARN', 'ERROR' ];
 let level = typeof window === 'undefined'
-	? ( process.env?.LOG ? process.env.LOG : 'ERROR' )
-	: ( localStorage?.LOG ? localStorage?.LOG : 'ERROR' );
+	? ( process.env?.LOG ? process.env.LOG : 'INFO' )
+	: ( localStorage?.LOG ? localStorage?.LOG : 'INFO' );
 
 const _log = a => ( ...b ) => {
 	if( levels.indexOf( a ) > -1 && levels.indexOf( a ) >= levels.indexOf( level ) ) {
 		//const location = new Error().stack.split('\n')[3].match(/(at \S*).*\/(\S*)\)/)
 		const lineNumber = level === 'DEBUG' ? basename( new Error().stack.split('\n')[3].slice(0,-1) )
 			.replace(/:\d+$/,'') : '';
-		console.log( `${ new Date().toLocaleString( 'sv-SE' ) } [ ${ a } ]`, lineNumber, b.join( ' ' ) );
+		console.log( `${ new Date().toLocaleString( 'sv-SE' ) } [ ${ a } ] ${ lineNumber } ${ b.join( ' ' ) }` );
 	}
 };
 
